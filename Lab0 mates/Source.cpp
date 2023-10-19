884addd75eed066747eaeda2b2aa5b6132410d79
DavidLastra8 <david.lastra@estudiantat.upc.edu>
2023-10-19 11:06:22 +0200

    bucle de 1 añadido(se sale de la array)

--- Lab0 mates/Source.cpp
+++ Lab0 mates/Source.cpp
@@ -41,7 +41,7 @@ int main() {
     if (typeGauss == 3) {
         for (int Mati = 0; Mati <= sz - 1; Mati++) {
 
-            for (int Matj = Mati + 1; Matj <= sz-1; Matj++) {
+            for (int Matj = Mati + 1; Matj <= sz - 1; Matj++) {
                 double fact = Mat[Matj][Mati] / Mat[Matj][Matj];
 
                 for (int Matk = Mati; Matk <= sz - 1; Matk++) {
@@ -58,18 +58,18 @@ int main() {
             }
             solucion[i] /= Mat[i][i];
         }
-      
+
         cout << "Solucion del sistema:" << endl;
         for (int i = 0; i < sz; i++) {
             cout << "x" << i + 1 << " = " << solucion[i] << endl;
         }
-        
+
         delete[] solucion;
 
         auto stop = chrono::high_resolution_clock::now();
         auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
         cout << "duration: " << duration.count() << " milliseconds" << endl;
-    
+
         for (int i = 0; i < sz; i++) {
             delete[] Mat[i];
         }
@@ -98,27 +98,40 @@ int main() {
         for (int i = 0; i < sz; i++) {
             cout << "x" << i + 1 << " = " << solucion[i] << endl;
         }
-        
+
         delete[] solucion;
 
         auto stop = chrono::high_resolution_clock::now();
         auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
-        cout << "duration: " << duration.count() << " milliseconds"<<endl;
+        cout << "duration: " << duration.count() << " milliseconds" << endl;
         for (int i = 0; i < sz; i++) {
             delete[] Mat[i];
         }
         delete[] Mat;
 
         return 0;
-            
+
     }
+
     if (typeGauss == 1) {
 
         MatrixXd mat(sz, sz);
-        for (int i = 0; i < sz; i++) {
-            for (int j = 0; j < sz; j++) {
-                mat(i, j) = i * sz + j + 1;
+        Eigen::VectorXd b(sz);
+        for (int col = 0; col < sz - 1; col++) {
+            // Encontrar el elemento máximo en la columna actual
+            int max_row = mat.block(col, col, sz - col, 1).cwiseAbs().maxCoeff() + col;
+            int max_val = -1;
+            
+            // Intercambiar filas si es necesario
+            if (max_row != col) {
+                mat.row(max_row).swap(mat.row(col));
+                swap(b(max_row), b(col));
             }
+
+            // Realizar la eliminación de Gauss en la columna actual
+            VectorXd ratios = mat.block(col + 1, col, sz - col - 1, 1) / mat(col, col);
+            mat.block(col + 1, col, sz - col - 1, sz - col) -= ratios * mat.block(col, col, 1, sz - col);
+            b.segment(col + 1, sz - col - 1) -= ratios * b(col);
         }
         double* solucion = new double[sz];
         for (int i = sz - 1; i >= 0; i--) {
